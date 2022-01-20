65da7a54a17d1f1f74ee905ff3c1c002bd60fbfe
NotseMac <notse@NotseMacs-MacBook-Pro.local>
2022-01-21 00:43:50 +0530

    second commit

---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './main.css';
+
+export default function mains() {
+  return (
+    <div className="container"> 
+        <div className="text">hello world</div>
+    </div>
+  )
+  
+}
